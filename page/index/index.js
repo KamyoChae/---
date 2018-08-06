e7e5873a39aebcb7786c2d29892ca89eb9795458
Y_林泉 <32585968+KamyoChae@users.noreply.github.com>
2018-08-06 20:01:21 +0800

    Update index.js

--- page/index/index.js
+++ page/index/index.js
@@ -1,23 +1,18 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
-
 Page({
   data: {
     motto: '乔巴的一千零一个鬼故事',
     userInfo: '../../images/logo.jpg',
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-
   },
 
   getEvent:function(e){
     // 获取绑定的id 修改用户点击类型
     app.myType = e.target.id
-
     app.getList() // 用于测试 显示发送请求的结果
-
 
   },
   //事件处理函数
@@ -26,12 +21,4 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function() {
-   
-
-
-  },
-
-
-
 })
